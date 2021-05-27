a504732a021c726b0de17c14541239c553de14b7
JonathanCrider <13684007+JonathanCrider@users.noreply.github.com>
2021-05-27 12:20:26 -0400

    added more prof quotes

---
+++ client/quotesprof.js
@@ -0,0 +1,15 @@
+const quotesprof = [
+  "I'm gonna cheat a little bit.",
+  "You could end up...",
+  "Wouldn't it be nice...",
+  "Nothing is preventing you...",
+  "My name is Eric Katz, better known as Prof",
+  "I'm gonna cheat a little bit.",
+  "acme_db",
+  "Moe, Larry, Curly, Lucy, and Ethyl",
+  "{foo: 'bar', bazz: 'quq'}",
+  "We can just splat it out...",
+  "I'm gonna cheat a little bit."
+]
+
+export default quotesprof;
